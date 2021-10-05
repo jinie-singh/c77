119c3d46117d07df2096f04c8bf9906e82d75045
jinie-singh <anjani.1997mpvm@gmail.com>
2021-10-05 16:39:00 +0530

    added

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,56 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, SafeAreaView,StyleSheet, TouchableOpacity,Platform } from 'react-native';
 
 export default class HomeScreen extends Component {
     render() {
         return (
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}>
-                <Text>Home Screen!</Text>
+            <View style={styles.container}>
+                <SafeAreaView style = {styles.droidSafeArea}/>
+                <View style={styles.titleBar}>
+                    <Text style = {styles.titleText}>ISS TRACKER</Text>
+                </View>
+                <TouchableOpacity style={styles.routeCard}>
+                    <Text style={styles.routeText}> ISS Locstion</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.routeCard}>
+                    <Text style={styles.routeText}> Meteors</Text>
+                </TouchableOpacity>
+                
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    titleText: {
+        fontSize: 40,
+        fontWeight: "bold",
+        color: "white"
+    },
+    titleBar: {
+        flex: 0.15,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    routeCard: {
+        flex: 0.25,
+        marginLeft: 50,
+        marginRight: 50,
+        marginTop: 50,
+        borderRadius: 30,
+        backgroundColor: 'white'
+    },
+    routeText: {
+        fontSize: 35,
+        fontWeight: "bold",
+        color: "black",
+        marginTop: 75,
+        paddingLeft: 30
+    },
+})
